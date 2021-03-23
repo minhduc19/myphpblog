2694f169a097f40ae4ede9c6ecf07cddbc533335
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2021-03-23 12:10:47 +0000

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "test";
-echo "now";
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
