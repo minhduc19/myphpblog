a5c3c8643bbe83621866d66e3830fcaf9b49669f
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2021-03-24 15:20:14 +0000

    add community

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -5,23 +5,16 @@
 <p><small>Created: <?= $article->created->format(DATE_RFC850) ?></small></p>
 <p><?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?></p>
 
-
-<?= $this->Form->create($article, [
-    'type' => 'get',
-    'valueSources' => ['query', 'context'],
-    'type' => 'get']); ?>
-    <?= $this->Form->control('user_id', ['value' => $article->user_id]);?>
-
-<?= $this->Form->end(); ?>
 <h2>Reply to this post</h2>
 <?= $this->Form->create($reply, ['url' => ['controller' => 'articles','action' => 'reply']]);?>
 <?= $this->Form->control('user_id', ['type' => 'hidden', 'value' => $article->user_id]);?>
 <?= $this->Form->control('replyto', ['type' => 'hidden','value' => $article->id]);?>
-<?= $this->Form->control('title',['label' => false]);?>
+<?= $this->Form->control('title');?>
 <?= $this->Form->control('body', array(
     
     'id'=>'summernote'
    
 )); ?>
+<?= $this->Form->control('community',['label' => 'Publish to community']);?>
 <?= $this->Form->button(__('Reply')); ?>
 <?= $this->Form->end(); ?>
